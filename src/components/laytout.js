754a4917ee50fd1ba9e47ac6790c6798d558b7e1
Caio Santos <caio.f.santos@icloud.com>
2019-04-23 19:49:18 -0300

    Added scss plugin

--- src/components/laytout.js
+++ src/components/laytout.js
@@ -1,6 +1,12 @@
 import React from 'react';
+
+
+//Components
 import Header from './header';
 import Footer from './footer';
+
+//Styles
+import '../styles/main.scss';
 
 const Layout = (props) => {
     return (
